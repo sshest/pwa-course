fc5f1d7074b5725f9501f4cc199ff42cdbc85261
Sergey.Shestakov <sergey.shestakov@dataart.com>
2019-07-15 17:42:04 +0300

    Send push notifications from the server

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const cors = require('cors')({origin: true});
 const serviceAccount = require('./pwagram-key.json');
+const webpush = require('web-push');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -19,7 +20,27 @@ exports.storePostsData = functions.https.onRequest((req, res) => {
    image: req.body.image
   })
       .then(() => {
-       return res.status(201).json({message: 'Data stored', id: req.body.id})
+          webpush.setVapidDetails('mailto:sshest78@gmail.com',
+              'BOBheh-RHWtrqY8UgW9pWsOCDJY8_VQHNKH_imSuGNfhbTWI7sIUIj8oucG2e-OiZR_LvBELQoiifWzsQYI5clE',
+              'TSfRxQOWxMANsOLf_XQfqcL4oDx4m0qd32B8lK7iHjc');
+          return admin.database().ref('subscriptions').once('value');
+      })
+      .then((subscriptions) => {
+          subscriptions.forEach((sub) => {
+              const pushConfig = {
+                  endpoint: sub.val().endpoint,
+                  keys: {
+                      auth: sub.val().keys.auth,
+                      p2556dh: sub.val().keys.p256dh
+                  }
+              };
+              webpush.sendNotification(pushConfig, JSON.stringify({
+                  title: 'New post',
+                  content: 'New post added!'
+              }))
+                  .catch(console.log);
+          });
+          return res.status(201).json({message: 'Data stored', id: req.body.id})
       })
       .catch((err) => {
        res.status(500).json({error: err});
