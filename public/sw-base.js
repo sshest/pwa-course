7a9f8f2a2d4852d8e022f5b3345d51df92cd4276
Sergii.Shestakov <sshest78@gmail.com>
2019-07-16 21:59:34 +0300

    Add custom handler for fetching posts from the server

--- public/sw-base.js
+++ public/sw-base.js
@@ -10,7 +10,8 @@
  * and re-run your build process.
  * See https://goo.gl/2aRDsh
  */
-
+importScripts('/src/js/idb.js');
+importScripts('/src/js/utility.js');
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 self.addEventListener('message', (event) => {
@@ -39,5 +40,25 @@ workbox.routing.registerRoute(/.*(?:firebasestorage.googleapis)\.com.*$/, new wo
 workbox.routing.registerRoute('https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css', new workbox.strategies.StaleWhileRevalidate({
     cacheName: 'material-css'
 }));
+
+workbox.routing.registerRoute('https://us-central1-pwa-cource-project.cloudfunctions.net/', (args) => {
+    return fetch(args.event.request)
+        .then((res) => {
+            const clonedResponse = res.clone();
+            clearAllData('posts')
+                .then(() => {
+                    return clonedResponse.json()
+                })
+                .then((data) => {
+                    for (const key in data) {
+                        if (!data.hasOwnProperty(key)) {
+                            return;
+                        }
+                        writeData('posts', data[key]);
+                    }
+                });
+            return res;
+        });
+});
 
 workbox.precaching.precacheAndRoute([]);
